39669217c66618a0bd3162951525a1c0c0b63892
Leonid Prikul <leonid_prikul@list.ru>
2023-08-30 20:45:26 +0300

    1 step

---
+++ bin/gendiff.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import genDiff from '@hexlet/code';
+
+const diff = genDiff(filepath1, filepath2);
+console.log(diff);
---
+++ makefile
@@ -0,0 +1,5 @@
+install: #устанавливает модули
+	npm ci
+
+publish:
+	npm publish --dry-run
