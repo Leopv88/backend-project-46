5883c74fb192da33c80f9a7b522eab628213da1a
Leonid Prikul <leonid_prikul@list.ru>
2023-10-12 16:24:14 +0300

    ex.5 update 1

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,8 @@
 // @ts-check
-/*
-import parse from './src/parse.js';
 
+// import parse from '../src/parse.js';
+
+/*
 test('parse', () => {
     expect(parse('file1.json')).toEqual('');
   });
--- src/parse.js
+++ src/parse.js
@@ -1,13 +1,18 @@
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 const getFileExtension = (string) => string.split('.').at(-1);
 
 export default (filepath) => {
-  const absolutFilePath = path.resolve(process.cwd(), filepath);
-  switch (getFileExtension(absolutFilePath)) {
+  // const absolutFilePath = path.resolve(process.cwd(), filepath);
+  switch (getFileExtension(filepath)) {
     case 'json':
-      return JSON.parse(fs.readFileSync(absolutFilePath));
+      return JSON.parse(readFile(filepath));
     default:
       throw new Error('Неверный формат файла');
   }
