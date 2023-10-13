0706f8f3bd35b4d4414006cf125ce18912e6d1ec
Leonid Prikul <leonid_prikul@list.ru>
2023-10-13 11:39:03 +0300

    setup test coverage 1

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,18 +3,18 @@ import { readFile, getFileExtension } from '../src/fileUtilits.js';
 import genDiff from '../index.js';
 import parse from '../src/parse.js';
 
-const content = readFile('expected_file.json')
+const content = readFile('expected_file.json');
 
 test('genDiff', () => {
-    expect(genDiff('file1.json', 'file2.json')).toEqual(content);
-  });
+  expect(genDiff('file1.json', 'file2.json')).toEqual(content);
+});
 
 test('formatFile', () => {
-    expect(getFileExtension('file1.json')).toEqual('json');
-  });
+  expect(getFileExtension('file1.json')).toEqual('json');
+});
 
 test('parse', () => {
-    expect(() => {
-      parse('file1.txt');
-    }).toThrow(`Неверный формат файла`);
-  });
+  expect(() => {
+    parse('file1.txt');
+  }).toThrow('Неверный формат файла');
+});
