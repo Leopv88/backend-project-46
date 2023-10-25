3e67507e1cea5092f88439cfc49c46fa689810c4
Leonid Prikul <leonid_prikul@list.ru>
2023-10-25 17:36:11 +0300

    delete consolelog

--- src/fileUtilits.js
+++ src/fileUtilits.js
@@ -6,6 +6,5 @@ export const getFileExtension = (filename) => filename.split('.').at(-1);
 export const readFile = (filepath) => {
   const dirname = process.cwd();
   const fullPath = path.resolve(dirname, filepath);
-  console.log(fullPath);
   return fs.readFileSync(fullPath, 'utf-8');
 };
