017ff9b05120ff32818b5fe5a1910ef533ca8bc3
Leonid Prikul <leonid_prikul@list.ru>
2023-10-25 12:40:16 +0300

    delete path fixture

--- src/fileUtilits.js
+++ src/fileUtilits.js
@@ -1,3 +1,4 @@
+import { createDiffieHellmanGroup } from 'crypto';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -5,8 +6,7 @@ import { fileURLToPath } from 'url';
 export const getFileExtension = (filename) => filename.split('.').at(-1);
 
 export const readFile = (filepath) => {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
-  const fixturePath = path.join(__dirname, '..', '__fixtures__', filepath);
-  return fs.readFileSync(fixturePath, 'utf-8');
+  const dirname = process.cwd();
+  const fullPath = path.resolve(dirname , filepath);
+  return fs.readFileSync(fullPath, 'utf-8');
 };
