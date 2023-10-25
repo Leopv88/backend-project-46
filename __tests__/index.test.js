7d77ed2274ba0ea526c1f3c6df68a2dc0f58e810
Leonid Prikul <leonid_prikul@list.ru>
2023-10-25 15:29:36 +0300

    update test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,24 +1,32 @@
 // @ts-check
-import { readFile, getFileExtension } from '../src/fileUtilits.js';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import path from 'path';
+import { getFileExtension } from '../src/fileUtilits.js';
 import genDiff from '../src/index.js';
 import parse from '../src/parse.js';
 
-const expectedFileStylish = readFile('expected_file_stylish.txt');
-const expectedFilePlain = readFile('expected_file_plain.txt');
-const expectedFileJSON = readFile('expected_file_json.txt');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+const expectedFileStylish = readFixture('expected_file_stylish.txt');
+const expectedFilePlain = readFixture('expected_file_plain.txt');
+const expectedFileJSON = readFixture('expected_file_json.txt');
 
 test('genDiff', () => {
-  expect(genDiff('file1.json', 'file2.json', 'stylish')).toEqual(expectedFileStylish);
-  expect(genDiff('filepath1.yml', 'filepath2.yml', 'stylish')).toEqual(expectedFileStylish);
-  expect(genDiff('filepath1.yaml', 'filepath2.yaml', 'stylish')).toEqual(expectedFileStylish);
-  expect(genDiff('filepath1.yaml', 'filepath2.yaml', 'plain')).toEqual(expectedFilePlain);
-  expect(genDiff('filepath1.yaml', 'filepath2.yaml', 'json')).toEqual(expectedFileJSON);
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'stylish')).toEqual(expectedFileStylish);
+  expect(genDiff(getFixturePath('filepath1.yml'), getFixturePath('filepath2.yml'), 'stylish')).toEqual(expectedFileStylish);
+  expect(genDiff(getFixturePath('filepath1.yaml'), getFixturePath('filepath2.yaml'), 'stylish')).toEqual(expectedFileStylish);
+  expect(genDiff(getFixturePath('filepath1.yaml'), getFixturePath('filepath2.yaml'), 'plain')).toEqual(expectedFilePlain);
+  expect(genDiff(getFixturePath('filepath1.yaml'), getFixturePath('filepath2.yaml'), 'json')).toEqual(expectedFileJSON);
 });
 
 test('formatFile', () => {
   expect(getFileExtension('expected_file.txt')).toEqual('txt');
   expect(() => {
-    genDiff('file1.json', 'file2.json', 'newType');
+    genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'newType');
   }).toThrow('Output format is not correct');
 });
 
--- src/fileUtilits.js
+++ src/fileUtilits.js
@@ -1,12 +1,11 @@
-import { createDiffieHellmanGroup } from 'crypto';
 import fs from 'fs';
 import path from 'path';
-import { fileURLToPath } from 'url';
 
 export const getFileExtension = (filename) => filename.split('.').at(-1);
 
 export const readFile = (filepath) => {
   const dirname = process.cwd();
-  const fullPath = path.resolve(dirname , filepath);
+  const fullPath = path.resolve(dirname, filepath);
+  console.log(fullPath);
   return fs.readFileSync(fullPath, 'utf-8');
 };
